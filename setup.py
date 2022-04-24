dff46673d01b37d3500b304314bc5b4f7b6ea662
data_sanchez <tam2511@mail.ru>
2022-04-24 13:06:04 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -3,10 +3,20 @@ from os.path import join, dirname
 
 setup(
     name='xvideos',
-    version='0.1',
+    version='0.3',
+    author="Alexander Timofeev",
+    author_email="tam2511@mail.ru",
+    python_requires=">=3.6",
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tam2511/xvideos",
     install_requires=[
         'opencv-python'
-    ]
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
